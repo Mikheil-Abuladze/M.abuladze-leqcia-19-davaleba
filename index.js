da557b28285718f0bed499702f4ae04cdf905bf2
Mikheil-Abuladze <abuladzemikheil1995@gmail.com>
2025-02-28 13:29:26 +0400

    updated pictures and styling

--- index.js
+++ index.js
@@ -3,28 +3,31 @@ const characters = [
     first_name: "Harry",
     last_name: "Potter",
     house: "Gryffindor",
-    image: "http://hp-api.herokuapp.com/images/harry.jpg",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/d/d7/Harry_Potter_character_poster.jpg",
     actor: "Daniel Radcliffe",
   },
   {
     first_name: "Hermione",
     last_name: "Granger",
     house: "Gryffindor",
-    image: "http://hp-api.herokuapp.com/images/hermione.jpeg",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/d/d3/Hermione_Granger_poshttps://static.wikia.nocookie.net/characters/images/a/a5/Latest_%2810%https://miro.medium.com/v2/resize:fit:768/1*obDCz57X7D2Kd1XvcdI0Iw.jpeg29.jpg/revision/latest/scale-to-width-down/1200?cb=20141230074301ter.jpghthttps://i0.wp.com/thebookdragonchronicles.wordhttps://miro.medium.com/v2/resize:fit:1400/format:webp/1*obDCz57X7D2Kd1XvcdI0Iw.jpegpress.com/wp-content/uploads/2016/04/hermionemovie1.jpg?w=494&h=686&ssl=1tp://hp-https://imgix.bustle.com/rehost/2016/9/14/f4246d6d-be37-4fdc-875c-279afc2519f2.jpg?w=248&h=177&fit=crop&crop=faces&dpr=2api.herokuapp.com/images/hermione.jpeghtthttps://upload.wikimedia.org/wikipedia/en/d/d3/By https://harrypotter.fandom.com/f/p/3343172654596426636/r/3344233352138735836, Fair use, https://en.wikipedia.org/w/index.php?curid=12437770Hermione_Granger_poster.jpgps://upload.wikimedia.org/wikipedia/en/d/d3/Hermione_Granger_poster.jpg",
     actor: "Emma Watson",
   },
   {
     first_name: "Ron",
     last_name: "Weasley",
     house: "Gryffindor",
-    image: "http://hp-api.herokuapp.com/images/ron.jpg",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/5/5e/Ron_Weasley_poster.jpg/220px-Ron_Weasley_poster.jpg",
     actor: "Rupert Grint",
   },
   {
     first_name: "Draco",
     last_name: "Malfoy",
     house: "Slytherin",
-    image: "http://hp-api.herokuapp.com/images/draco.jpg",
+    image: "https://upload.wikimedia.org/wikipedia/en/1/16/Draco_Mal.JPG",
     actor: "Tom Felton",
   },
 ];
